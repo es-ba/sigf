c0beb9408a7bd382c9ca9639d2b5d5ec20634587
Emilio <emilioplatzer@gmail.com>
2019-09-24 16:13:48 -0300

    Agrego autonomía y dimensión a los indicadores

--- src/server/table-dimensiones.ts
+++ src/server/table-dimensiones.ts
@@ -9,8 +9,8 @@ export function dimensiones(context:TableContext):TableDefinition{
         editable: admin,
         fields: [
             {name: 'dimension'              ,typeName:'text'      ,nullable:false},
-            {name: 'agrupacion_principal'   ,typeName:'text'      ,nullable:false},
-            {name: 'denominacion'           ,typeName:'text'      },
+            {name: 'agrupacion_principal'   ,typeName:'text'      , isName:true,nullable:false},
+            {name: 'denominacion'           ,typeName:'text'      , title:'dimensión', isName:true},
             {name: 'orden'                  ,typeName:'integer'   },
             {name: 'ocultar'                ,typeName:'boolean'   },
             {name: 'icono'                  ,typeName:'text'      },
--- src/server/table-indicadores.ts
+++ src/server/table-indicadores.ts
@@ -8,29 +8,47 @@ export function indicadores(context:TableContext):TableDefinition{
         name:'indicadores',
         editable: puedeEditar,
         fields: [
-            {name:'dimension'                , label:'Dimensión'                             , typeName:'text' },
-            {name:'indicador'                , label:'Código indicador'                      , typeName:'text' ,nullable:false},
-            {name:'denominacion'             , label:'Denominación' , isName:true            , typeName:'text' },
-            {name:'orden'                    , label:'Orden'                                 , typeName:'integer' },
-            {name:'fte'                      , label:'Fuente'                                , typeName:'text'},
-            {name:'um'                       , label:'Unidad de medida'                      , typeName:'text'},
-            {name:'universo'                 , label:'Universo'                              , typeName:'text'},
-            {name:'def_con'                  , label:'Definición conceptual'                 , typeName:'text'},
-            {name:'def_ope'                  , label:'Definición operativa'                  , typeName:'text'},
-            {name:'cob'                      , label:'Cobertura'                             , typeName:'text'},
-            {name:'desagregaciones'          , label:'Desagregaciones'                       , typeName:'text'},
-            {name:'uso_alc_lim'              , label:'Uso - Alcance - Limitaciones'          , typeName:'text'},
-            {name:'metas'                    , label:'Metas'                                 , typeName:'text'},
-            {name:'ods'                      , label:'ODS'                                   , typeName:'text'},
+            {name:'autonomia'                , title:'Autonomía'                             , typeName:'text'      , isName:true,nullable:false},
+            {name:'orden_autonomia'                                                          , typeName:'integer' },
+            {name:'orden_dimension'                                                          , typeName:'integer' },
+            {name:'dimension'                , title:'Dimensión'                             , typeName:'text' },
+            {name:'indicador'                , title:'Código indicador'                      , typeName:'text' ,nullable:false},
+            {name:'denominacion'             , title:'indicador'                             , typeName:'text' , isName:true},
+            {name:'orden'                    , title:'Orden'                                 , typeName:'integer' },
+            {name:'fte'                      , title:'Fuente'                                , typeName:'text'},
+            {name:'um'                       , title:'Unidad de medida'                      , typeName:'text'},
+            {name:'universo'                 , title:'Universo'                              , typeName:'text'},
+            {name:'def_con'                  , title:'Definición conceptual'                 , typeName:'text'},
+            {name:'def_ope'                  , title:'Definición operativa'                  , typeName:'text'},
+            {name:'cob'                      , title:'Cobertura'                             , typeName:'text'},
+            {name:'desagregaciones'          , title:'Desagregaciones'                       , typeName:'text'},
+            {name:'uso_alc_lim'              , title:'Uso - Alcance - Limitaciones'          , typeName:'text'},
+            {name:'metas'                    , title:'Metas'                                 , typeName:'text'},
+            {name:'ods'                      , title:'ODS'                                   , typeName:'text'},
         ],
         primaryKey:['indicador'],
         foreignKeys:[
-            // {references:'dimension', fields:['dimension']},
+            {references:'dimensiones', fields:['dimension']},
             // {references:'fte'  , fields:['fte']},
             // {references:'um'   , fields:['um']}
         ],
         detailTables:[
             {table: 'jur_ind', fields:['indicador'], abr:'J', label:'jurisdicciones'},
-        ]
+        ],
+        sql:{
+            fields:{
+                autonomia:{
+                    expr:`(select a.denominacion from agrupacion_principal a inner join dimensiones d using (agrupacion_principal) where d.dimension = indicadores.dimension)`
+                },
+                orden_autonomia:{
+                    expr:`(select a.orden from agrupacion_principal a inner join dimensiones d using (agrupacion_principal) where d.dimension = indicadores.dimension)`
+                },
+                orden_dimension:{
+                    expr:`(select d.orden from dimensiones d where d.dimension = indicadores.dimension)`
+                }       
+            }
+        },
+        hiddenColumns:['indicador','orden_autonomia', 'orden_dimension','agrupacion_principal','dimension','orden'],
+        sortColumns:[{column:'orden_autonomia'},{column:'orden_dimension'},{column:'orden'}]
     };
 }
